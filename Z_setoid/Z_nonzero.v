e07d872896ba27aff845838311f63ab2d51d3d48
Fennec Fox <41368919+fennecf0x@users.noreply.github.com>
2018-08-08 20:58:29 +0900

    Update Z_nonzero.v

--- Z_setoid/Z_nonzero.v
+++ Z_setoid/Z_nonzero.v
@@ -1,4 +1,5 @@
 From Coq Require Import Arith.
+Require Import Coq.omega.Omega.
 Require Import logic.
 Require Import natural.
 Require Import integer.
@@ -6,7 +7,7 @@ Require Import ProofIrrelevance.
 
 Local Coercion is_true : bool >-> Sortclass.
 
-Definition Z_nonzero: Set := {m : integer | negb (Z_eqb m Z0) }.
+Definition Z_nonzero: Set := {m : integer | ~ m =Z= Z0 }.
 
 Definition Z_nonzero__Z (p: Z_nonzero): integer := proj1_sig p.
 
@@ -14,14 +15,10 @@ Definition Z_nonzero_mult : Z_nonzero -> Z_nonzero -> Z_nonzero.
   intros [x xnz] [y ynz].
   refine (exist  _ (x *Z y) _).
   unfold is_true in xnz, ynz.
-  rewrite <- negb_true in xnz, ynz.
   destruct x, y.
-  unfold Z_eqb in xnz, ynz. rewrite N_eqb_false__ne in xnz, ynz.
+  unfold Z_eq in xnz, ynz.
   zero_in xnz; zero_in ynz.
-  unfold is_true.
-  rewrite <- negb_true.
-  unfold Z_eqb. simpl. zero.
-  rewrite N_eqb_false__ne.
+  unfold Z_eq. simpl. zero.
   rewrite N_trichotomy_ne.
   rewrite N_trichotomy_ne in xnz, ynz.
   destruct xnz; destruct ynz.
@@ -34,16 +31,9 @@ Definition Z_nonzero_mult : Z_nonzero -> Z_nonzero -> Z_nonzero.
 Defined.
 
 Lemma Z_nonzero_mult_compat:
-forall p q: Z_nonzero, Z_nonzero__Z (Z_nonzero_mult p q) = Z_nonzero__Z p *Z Z_nonzero__Z q.
+forall p q: Z_nonzero, Z_nonzero__Z (Z_nonzero_mult p q) =Z= Z_nonzero__Z p *Z Z_nonzero__Z q.
 Proof. now intros [x xnz] [y ynz]. Defined.
 
 Definition ZN1: Z_nonzero.
   refine (exist  _ Z1 _). easy.
-Defined.
-
-Lemma Z_nonzero__Z_injective:
-  forall t1 t2: Z_nonzero, Z_nonzero__Z t1 = Z_nonzero__Z t2 -> t1 = t2.
-Proof.
-  intros [x1 i1] [x2 i2]; simpl; intros e.
-  rewrite (subset_eq_compat _ _ x1 x2 i1 i2); try reflexivity; apply e.
 Defined.
