69223d178acf7bc10a50dc28e24a2faf1bce7647
Fennec Fox <41368919+fennecf0x@users.noreply.github.com>
2018-08-08 19:24:54 +0900

    Update logic.v

--- Z_setoid/logic.v
+++ Z_setoid/logic.v
@@ -2,3 +2,7 @@ Lemma contrapositive: forall P Q: Prop, (P -> Q) -> (~Q -> ~P).
 Proof.
   intros. unfold not. intros. apply H in H1. contradiction.
 Qed.
+
+Lemma negb_true: forall b: bool, b = false <-> negb b = true.
+Proof. intros; split; intros. rewrite H. reflexivity. destruct b. inversion H. reflexivity.
+Defined.
